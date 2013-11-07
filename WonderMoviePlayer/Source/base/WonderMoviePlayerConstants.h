c63bdbce69d3858fcfb899f0c651821066208cf8
Yanjun Zhuang <zhuangyanjun@gmail.com>
2013-11-07 15:20:45 +0800

    bugfix

--- WonderMoviePlayer/Source/base/WonderMoviePlayerConstants.h
+++ WonderMoviePlayer/Source/base/WonderMoviePlayerConstants.h
@@ -18,6 +18,17 @@
 #define QQVideoPlayerImage(src) [UIImageEx loadAndCacheImageFromApp:[NSString stringWithFormat:@"function_videoplayer_%@", src]]
 #endif
 
+// define colors
+#define RGBAColor(r, g, b, a) [UIColor colorWithRed:r / 255.f green:g / 255.f blue:b / 255.f alpha:a / 255.f];
+#define QQColor(colorName) colorName
+
+#ifndef QQColor
+#define QQColor(colorName) Color(#colorName)
+#endif
+
+#define videoplayer_title_color     RGBAColor(0xb2, 0xb2, 0xb2, 0xff)
+#define videoplayer_subtitle_color  RGBAColor(0x8d, 0x8d, 0x8d, 0xff)
+
 #define kProgressViewPadding 16
 #define kWonderMovieControlDimDuration              0.8f
 #define kWonderMovieControlShowDuration             0.2f
